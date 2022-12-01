8593e3e4db928e1d45888cf8355830ce1e79417a
nckedev <85008195+nckedev@users.noreply.github.com>
2022-12-01 16:50:02 +0100

    timer, wildcard

--- aoc2022/DayBase.cs
+++ aoc2022/DayBase.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using System.Numerics;
-using System.Windows;
 
 namespace aoc2023;
 
@@ -22,19 +21,31 @@ public abstract class DayBase<T> where T : INumber<T>
 }
 
 
-public static class Solver 
+public static class Solver
 {
+    private static long totalTime;
     public static void Solve<T>(DayBase<T> day) where T : INumber<T>
     {
         Stopwatch s = new Stopwatch();
         s.Start();
-        Console.Write("1: " + day.Solve());
-        Console.WriteLine("    in " + s.Elapsed.Milliseconds + "ms");
+        var res = day.Solve();
         s.Stop();
+        Console.Write("1: " + res);
+        Console.WriteLine("    in " + s.Elapsed.Milliseconds + "ms");
+        totalTime += s.Elapsed.Milliseconds;
         s.Restart();
-        Console.Write("2: " + day.Solve2());
-        Console.WriteLine("    in " + s.Elapsed.Milliseconds + "ms");
+        var res2 = day.Solve2();
         s.Stop();
+        Console.Write("2: " + res2);
+        Console.WriteLine("    in " + s.Elapsed.Milliseconds + "ms");
+        totalTime += s.Elapsed.Milliseconds;
+        
+        Console.WriteLine("total Time: " + totalTime + "ms");
+    }
+
+    public static void SolveAll()
+    {
+        Console.WriteLine("not implemented");
     }
 }
 
