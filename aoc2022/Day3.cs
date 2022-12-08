875f7534a925082d60f7a3b5c5ccb475e049a576
nckedev <85008195+nckedev@users.noreply.github.com>
2022-12-08 08:22:53 +0100

    day3 **

--- aoc2022/Day3.cs
+++ aoc2022/Day3.cs
@@ -1,35 +1,92 @@
 ﻿namespace aoc2023;
 
-public class Day3 : DayBase<int>
+public sealed class Day3 : DayBase<ulong>
 {
-    private long first;
-    private long second;
+    private ulong first;
+    private ulong second;
+    private List<string> list;
 
-    public override int Solve()
+    public Day3()
     {
-        var list = ReadFileAsLines(3).ToList();
+        list = ReadFileAsLines(3).ToList();
+    }
+
+    public override ulong Solve()
+    {
+        var res = 0UL;
+
         foreach (var row in list)
         {
-            for (int i = 0; i < row.Count(); i++)
+            for (var i = 0; i < row.Length; i++)
             {
-                if (i <= row.Length / 2)
+                if (i < row.Length / 2)
                 {
-                    first |= (uint) (1 << (row[i] - 'a'));
+                    first |= 1UL << CharToIndex(row[i]);
                 }
                 else
                 {
-                    second |= (uint) (1 << (row[i] - 'a'));
+                    second |= 1UL << CharToIndex(row[i]);
                 }
             }
 
-            var res = first & second;
-            var b = 1;
+            res += (ulong) Math.Log2(first & second) + 1;
+            first = second = 0;
         }
-        return 0;
+
+        return res;
     }
 
-    public override int Solve2()
+
+    private void PrintBinary(ulong value)
     {
-        return 0;
+        var str = "";
+        var hasAtleastOneBitSet = false;
+        for (var i = 0; i < 64; i++)
+        {
+            if ((value & (1UL << i)) != 0)
+            {
+                str += "1";
+                hasAtleastOneBitSet = true;
+            }
+            else
+            {
+                str += "0";
+            }
+        }
+
+        if (!hasAtleastOneBitSet) throw new Exception("ingen etta satt");
+        Console.WriteLine(str);
+    }
+
+    //a = 0, Z = 51
+    private static int CharToIndex(char c) =>
+        char.IsUpper(c) ? c - 'A' + 26 : c - 'a';
+
+    public override ulong Solve2()
+    {
+        var count = 1;
+        var ans = ulong.MaxValue;
+        var t = 0UL;
+        var score = 0;
+        foreach (var listrow in list)
+        {
+            for (var i = 0; i < listrow.Length; i++)
+            {
+                t |= 1UL << CharToIndex(listrow[i]);
+            }
+
+            ans &= t;
+            t = 0UL;
+
+            if (count % 3 == 0)
+            {
+                score += (int) Math.Log2(ans) + 1;
+                ans = ulong.MaxValue;
+            }
+
+            count++;
+        }
+
+        return (ulong) score;
     }
 }
